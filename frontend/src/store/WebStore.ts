da10095bc65f6ecdab63766c333d76762337f4ed
Favour E. Onosemuode <favejakp@gmaill.com>
2025-10-05 02:14:05 +0100

    Customized feed

--- frontend/src/store/WebStore.ts
+++ frontend/src/store/WebStore.ts
@@ -8,17 +8,17 @@ export const useWebStore: Function = defineStore('WebStore', {
             {
                 id: Math.floor((Math.random() * 10000) + 1),
                 image: '/newspaper.svg',
-                author: 'Joshua Curtey',
+                author: 'Victor B. Boulhavkh',
                 date: '25 Oct',
-                title: 'The Junior Developer\'s Dilemma',
-                information: 'We have seen students bother about their pcs and we have tried to help them out. Many of them have problems with their personal workstation and it is no good. I know there is more in them but with limited resources they can only do much'
+                title: 'The B Team',
+                info: 'We have seen students bother about their pcs and we have tried to help them out. Many of them have problems with their personal workstation and it is no good. I know there is more in them but with limited resources they can only do much'
             },
             {
                 id: Math.floor((Math.random() * 10000) + 1),
                 image: '/newspaper.svg',
-                author: 'Joshua Curtey',
+                author: 'Haeman Freedmond',
                 date: '25 Oct',
-                title: 'The Junior Developer\'s Dilemma',
+                title: 'Taking after the best, not witstanding',
                 info: 'We have seen students bother about their pcs and we have tried to help them out. Many of them have problems with their personal workstation and it is no good. I know there is more in them but with limited resources they can only do much'
             },
             {
@@ -32,9 +32,25 @@ export const useWebStore: Function = defineStore('WebStore', {
             {
                 id: Math.floor((Math.random() * 10000) + 1),
                 image: '/newspaper.svg',
-                author: 'Joshua Curtey',
+                author: 'Berner Seaman',
+                date: '27 Oct',
+                title: 'Taking a Senior Developer role',
+                info: 'Today we have alot of vibe coders and what do you know, you might be one. Many of them have problems with their personal workstation and it is no good. I know there is more in them but with limited resources they can only do much'
+            },
+            {
+                id: Math.floor((Math.random() * 10000) + 1),
+                image: '/newspaper.svg',
+                author: 'Victor B. Boulhavkh',
                 date: '25 Oct',
-                title: 'The Junior Developer\'s Dilemma',
+                title: 'The B Team',
+                info: 'We have seen students bother about their pcs and we have tried to help them out. Many of them have problems with their personal workstation and it is no good. I know there is more in them but with limited resources they can only do much'
+            },
+            {
+                id: Math.floor((Math.random() * 10000) + 1),
+                image: '/newspaper.svg',
+                author: 'Haeman Freedmond',
+                date: '25 Oct',
+                title: 'Taking after the best, not witstanding',
                 info: 'We have seen students bother about their pcs and we have tried to help them out. Many of them have problems with their personal workstation and it is no good. I know there is more in them but with limited resources they can only do much'
             }
         ]
--- frontend/src/views/Feed.vue
+++ frontend/src/views/Feed.vue
@@ -1,54 +1,84 @@
 <script setup lang="ts">
-import Impression from '../components/Impression.vue'
-import { useWebStore } from '../store/WebStore.ts'
-const webStore = useWebStore()
+import Impression from "../components/Impression.vue";
+import Home from "../views/Home.vue";
+import { useWebStore } from "../store/WebStore.ts";
+const webStore = useWebStore();
 </script>
 <template>
-    <section class="flex flex-col bg-white">
-        <nav class="flex justify-between p-2 bg-blue-700">
-            <div class="flex space-x-1 w-fit">
-                <div class="h-10 w-10 rounded-full">
-                    <img src="/space-awesome.svg" class="h-full w-full" />
-                </div>
-                <p class="text-sm text-white">Musa-Carlos Space App</p>
+  <section class="flex bg-blue-600">
+    <nav class="flex flex-col space-y-1 p-2">
+      <div class="flex space-x-1 w-fit items-center">
+        <div class="h-10 w-10 rounded-full">
+          <img src="/space-awesome.svg" class="h-full w-full" />
+        </div>
+        <p class="text-sm text-white">Username</p>
+      </div>
+      <img src="../assets/bars.svg" class="hidden h-10 w-10 filter invert-90" />
+      <p
+        class="text-3xl text-white font-semibold rounded-full hover:text-zinc-400"
+      >
+        x
+      </p>
+      <ul class="text-white w-full">
+        <li
+          class="p-1 delay-[500] hover:bg-gradient-to-r hover:from-zinc-200 hover:to-blue-200 hover:text-blue-600"
+        >
+          Feed
+        </li>
+        <li
+          class="p-1 delay-[500] hover:bg-gradient-to-r hover:from-zinc-200 hover:to-blue-200 hover:text-blue-600"
+        >
+          Settings
+        </li>
+        <li
+          class="p-1 delay-[500] hover:bg-gradient-to-r hover:from-zinc-200 hover:to-blue-200 hover:text-blue-600"
+        >
+          Saved
+        </li>
+      </ul>
+    </nav>
+    <ul class="flex flex-col h-[700px] w-full space-y-1 bg-white overflow-scroll">
+      <li
+        v-for="news in webStore.news"
+        :key="news.id"
+        class="rounded-xl m-2 shadow-sm p-2 w-1/3 space-y-2"
+      >
+        <!-- These are default data from store, on MongoDB we'll create the complex stuff there after studying the Mongo Schema to Backend interaction-->
+        <div class="flex justify-between">
+          <div class="flex space-x-1">
+            <div class="h-5 w-5 rounded-full">
+              <img :src="news.image" class="h-full w-full" />
             </div>
-            <img src="../assets/bars.svg" class="h-10 w-10" />
-        </nav>
-        <ul class="flex flex-col space-y-1">
-            <li v-for="news in webStore.news" :key="news.id">
-            <!-- These are default data from store, on MongoDB we'll create the complex stuff there after studying the Mongo Schema to Backend interaction-->
-                <div class="flex justify-between">
-                    <div class="flex space-x-1">
-                        <div class="h-5 w-5 rounded-full">
-                            <img :src="news.image" class="h-full w-full" />
-                        </div>
-                        <div class="text-sm flex flex-col">
-                            <p class="font-semibold">{{ news.author }}</p>
-                            <p class="text-gray-400">{{ news.date }}</p>
-                        </div>
-                    </div>
-                    <div class="flex space-x-1">
-                        <p class="px-3 py-1 rounded-full"></p>
-                        <img src="/down-left-and-up-right-to-center.svg" class="h-5 w-5" />
-                    </div>
-                </div>
-                <div class="flex flex-col text-sm space-y-2">
-                    <p class="text-white font-semibold">{{ news.title }}</p>
-                    <p class="text-gray-400 font-thin">{{ news.information }}</p>
-                </div>
-                <Impression />
-            </li>
-        </ul>
-        <footer class="fixed bottom-0 left-0 w-full flex justify-center items-center space-x-5">
-            <div class="h-10 w-10">
-                <img src="/home-alt.svg" class="h-full w-full" />
+            <div class="text-sm flex flex-col">
+              <p class="font-semibold">{{ news.author }}</p>
+              <p class="text-gray-400">{{ news.date }}</p>
             </div>
-            <div class="h-10 w-10">
-                <img src="/bookmark.svg" class="h-full w-full" />
-            </div>
-            <div class="h-10 w-10">
-                <img src="/grin-beam.svg" class="h-full w-full" />
-            </div>
-        </footer>
-    </section>
+          </div>
+          <div class="flex space-x-1">
+            <p class="px-3 py-1 rounded-full"></p>
+            <img src="/down-left-and-up-right-to-center.svg" class="h-5 w-5" />
+          </div>
+        </div>
+        <div class="flex flex-col text-sm space-y-2">
+          <p class="text-gray-600 font-semibold">{{ news.title }}</p>
+          <p class="text-gray-400 font-thin">{{ news.info.split(".")[0] }}</p>
+        </div>
+        <Impression />
+      </li>
+    </ul>
+    <footer
+      class="fixed bottom-0 left-0 w-full flex justify-center items-center space-x-5"
+    >
+      <div class="h-10 w-10">
+        <img src="/home-alt.svg" class="h-full w-full" />
+      </div>
+      <div class="h-10 w-10">
+        <img src="/bookmark.svg" class="h-full w-full" />
+      </div>
+      <div class="h-10 w-10">
+        <img src="/grin-beam.svg" class="h-full w-full" />
+      </div>
+    </footer>
+    <Home class="hidden lg:flex w-1/3" />
+  </section>
 </template>
